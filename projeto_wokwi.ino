1f00936e56f418519dd7063fa470e236a766e546
Fernando Gurgel <2016118970116@ifam.edu.br>
2025-10-06 20:25:20 -0300

    Adds initial button reading function
    Adds a function to read a button press on a given pin, ensuring the button is released before returning.
    
    This function, `ler_botao_P`, is intended to provide a reliable way to detect button presses by waiting for the button to be fully released after it's initially pressed.

---
+++ projeto_wokwi.ino
@@ -0,0 +1,21 @@
+// Criar função para ligar a bomba
+
+// Funcao para anlisar o ph
+
+// Funcao para anlisar o humidade
+
+// funcao do botao se está precionado
+bool ler_botao_P(uint8_t pin){
+  uint8_t leitura = LOW;
+  do{
+    uint8_t leitura = digitalRead(pin)
+  } while(leitura == HIGH)
+
+  return true;
+}
+// Funcao Ler botão P
+
+// Funcao Ler botão K
+
+// Funcao Ler botão N
+
